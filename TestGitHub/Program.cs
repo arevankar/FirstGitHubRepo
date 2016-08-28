929aa33e1d4857067b04df3a37f3440c6714a053
arevankar <ashrevtech@gmail.com>
2016-08-27 20:36:08 -0400

    My first commit

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -10,7 +10,7 @@ namespace TestGitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
         }
     }
 }
